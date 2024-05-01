2a379f74eaf81634a047881659b6707fb9684319
TK-99 <keeblet@gmail.com>
2024-05-01 21:08:57 +1000

    Initial Commit

---
+++ LittleLemonRestaurant/LittleLemonRestaurantApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonRestaurantApp.swift
+//  LittleLemonRestaurant
+//
+//  Created by tony on 1/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonRestaurantApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
